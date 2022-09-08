7359251042464d5828082fe62c76cc09f8f44d40
enlyth <popik.boris@gmail.com>
2022-09-08 13:05:01 +0100

    Lerna changes

--- packages/tn1gg-web/components/layout/Navigation.tsx
+++ packages/tn1gg-web/components/layout/Navigation.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { ExternalLink } from "../../icons/ExternalLink";
 import { GitHubLogo } from "../../icons/GitHub";
@@ -8,13 +9,15 @@ import { Button } from "../generic/Button";
 export const Navigation: React.FC = () => {
   return (
     <div className="flex flex-row justify-between items-center w-full bg-indigo-700 py-2 px-3 shadow-md">
-      <Image
-        src="/logo.png"
-        alt="logo"
-        width="100%"
-        height={24}
-        objectFit="contain"
-      />
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width="100%"
+          height={24}
+          objectFit="contain"
+        />
+      </Link>
       <div className="flex flex-row items-center">
         <a
           href="https://webmail.tn1.gg"
